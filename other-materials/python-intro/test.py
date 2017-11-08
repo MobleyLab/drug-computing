892c7f00596f0bde3ad157502353cf77a9e9aec7
David Mobley <dmobley@gmail.com>
2017-11-08 15:27:30 -0800

    Add test.py updated version

--- other-materials/python-intro/test.py
+++ other-materials/python-intro/test.py
@@ -1,6 +1,6 @@
 def multiply(x, y):
-    try:
-        ret = x * y
-    except StandardError:
-        ret = 0
-    return ret
+    return x*y
+
+if __name__ == "__main__":
+    #only executed if run directly from the command line
+    print(multiply(4, 5))
